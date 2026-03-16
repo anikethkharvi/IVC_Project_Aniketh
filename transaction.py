809e8e836dc4aee414bb522d48a1d6fe66d5fa85
anikethkharvi <kharviani2@gmail.com>
2026-03-16 12:17:41 +0530

    feat: initialize base ledger module

---
+++ transaction.py
@@ -0,0 +1,10 @@
+# Transaction Module
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+total = a + b
+difference = a - b
+
+print("Total:", total)
+print("Difference:", difference)
